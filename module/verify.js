384b48e47abc5c31205753d994698b89f3d4e988
kelly0924 <hyj09240@gmail.com>
2022-07-16 09:16:00 +0000

    CREATE: SINGUP API AND CALL_ACCOUNT_INFO AND DUFLICATE_ID create api singup api and impormation account api and account id dupulicate api

--- module/verify.js
+++ module/verify.js
@@ -6,20 +6,16 @@ const secretKey=process.env.TOKEN_SCREAT_KEY
 
 const token_verify=(pulbic_key)=>{
 
-    const result={
-        "success":false,
-        "message":null
-    }
+    const success =false
 
     try{
         jwt.verify(tokenValue,secretKey)//서버가 가지고 있는 secretKey로 검증한다.
-        result.success=true
-        res.send(result)
+        success=true
     }catch(e){
         result.message="토큰이 잘못 됬음"
     }
 
-    return result
+    return success
 }
 
 module.exports=token_verify
--- router/account.js
+++ router/account.js
@@ -5,6 +5,7 @@ const {Client}=require("pg")
 require("dotenv").config()
 const logFuntion=require("../module/logging")
 const moment=require("../module/moment")
+const tokenVerify=require("../module/verify")
 
 const secretKey=process.env.TOKEN_SCREAT_KEY
 
@@ -18,7 +19,7 @@ router.post("/login",(req,res)=>{
    const api_call_time=moment()
 
     const result={
-        "succeed":false,
+        "success":false,
         "message":"",
         "token":""
     }
@@ -29,8 +30,8 @@ router.post("/login",(req,res)=>{
         if(err)
             console.log(err)
     })
-    console.log(secretKey)
-    const sql="SELECT * FROM  badonnaproject.account WHERE userid=$1 and userpw=$2"
+   
+    const sql="SELECT * FROM  badonnaproject.account WHERE id=$1 and pw=$2"
     const values=[idValue,pwValue]
 
     // db.qeury(sql,values,(err,data)=>{
@@ -53,7 +54,7 @@ router.post("/login",(req,res)=>{
     //             // )
                 
     //             // result.token=jwtToken
-    //             result.succeed=true
+    //             result.success=true
                 
     //         }
 
@@ -64,15 +65,146 @@ router.post("/login",(req,res)=>{
     //     db.end()
 
     // })
-
-    console.log( "api time", api_call_time)
-
-    res.send(result)
    
 })
 
+//로그 아웃
+
+router.post("/account/logout",(req,res)=>{
+
+    const token_public=req.headers.token
+    const idValue=req.body.id
+
+    const result={
+        "success":false,
+        "message":""
+    }
+    //token 인증 
+    if(tokenVerify(token_public)){//token 인증이 성공 한 경우
+        //발급 해준 public_key를 삭제?
+
+    }
+  
 
 
+})
+
+//회원 가입
+
+router.post("/",(req,res)=>{
+
+    const idValue=req.body.id
+    const pwValue=req.body.pw
+    const user_name=req.body.name
+    const user_birth=req.body.birth 
+    const join_date=req.body.data
+
+    const api_name=req.url
+    const req_host=req.headers.req_host
+    const api_call_time=moment()
+
+    const result={
+        "success":false,
+        "message":""
+    }
+
+    const db=new Client(pgInit)
+    db.connect((err)=>{
+        if(err)
+            console.log("db connect",err)
+    })
+ 
+    const sql="INSERT INTO badonnaproject.account(id,pw,name, phonenum, date) VALUES($1,$2,$3,$4,$5)"
+    const values=[idValue,pwValue,user_name,user_birth,join_date]
+    db.query(sql,values,(err,rows)=>{
+        if(!err){
+            result.success=true
+        }else{
+            console.log(err)
+        }
+
+        //logging 
+        logFuntion(idValue, api_name,req_host,api_call_time)
+         
+        res.send(result)
+        db.end()
+    })
+
+})
+
+
+router.get("/",(req,res)=>{
+    
+    const idValue=req.query.id 
+    const token_public=req.headers.token
+     
+    const result={
+        "success":false,
+		"data":null
+    }
+//token verify 
+    if(tokenVerify(token_public)){
+
+        const db=new Client(pgInit)
+        db.connect((err)=>{
+            if(err)
+                console.log(err)
+        })
+
+        const sql="SELECT * FROM  badonnaproject.account WHERE userid=$1"
+        const values=[idValue]
+        db.query(sql,values,(err,row)=>{
+            if(!err){
+                result.success=true
+                result.data=row
+            }else{
+                console.log(err)
+            }
+            
+            res.send(result)
+            db.end()
+        })
+    }
+    
+
+})
+
+//중복 아이디 체크 
+router.post("/account/duplicate/id",(req,res)=>{
+    const idValue=req.body.id
+
+   const api_name=req.url
+   const req_host=req.headers.req_host
+   const api_call_time=moment()
+
+    const result={
+        "success":false
+    }
+    
+    //db 연결
+    const db=new Client(pgInit)
+    db.connect((err)=>{
+        if(err)
+            console.log(err)
+    })
+   
+    const sql="SELECT * FROM  badonnaproject.account WHERE id=$1"
+    const values=[idValue]
+
+    db.qeury(sql,values,(err,data)=>{
+        if(!err){
+            const row=data.rows
+            if(row == idValue)
+                result.success=true
+            else
+                result.success=false
+        }
+
+        res.send(result)
+        db.end()
+    })
+
+})
 
 
 module.exports=router
